5204c7b02cb89dcafc71f981e35dcd3230739dde
ThanhDanhhh <danhdang270@gmail.com>
2024-06-27 22:48:58 +0700

    add ui login

--- src/routers/Router.tsx
+++ src/routers/Router.tsx
@@ -1,23 +1,42 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import {View, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from '../screens/homes/HomeScreen';
 import AddNewTask from '../screens/tasks/AddNewTask';
 import SearchScreen from '../screens/SearchScreen';
+import auth from '@react-native-firebase/auth';
+import LoginScreen from '../auth/LoginScreen';
 
 const Router = () => {
-    const Stack = createNativeStackNavigator();
-  return (
-    <Stack.Navigator
-    screenOptions={{
-        headerShown: false,
-    }}>
-        <Stack.Screen name="HomeScreen" component={HomeScreen}/>
-        <Stack.Screen name="AddNewTask" component={AddNewTask}/>
-        <Stack.Screen name="SearchScreen" component={SearchScreen}/>
-    </Stack.Navigator>
-    
-  )
-}
+  const [isLogin, setIsLogin] = useState(false);
+  useEffect(() => {
+    auth().onAuthStateChanged(user => {
+      if (user) {
+        setIsLogin(true);
+      } else {
+        setIsLogin(false);
+      }
+    });
+  }, []);
+  const Stack = createNativeStackNavigator();
 
-export default Router
+  const MainRouter = (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}>
+      <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      <Stack.Screen name="AddNewTask" component={AddNewTask} />
+      <Stack.Screen name="SearchScreen" component={SearchScreen} />
+    </Stack.Navigator>
+  );
+
+  const AuthRouter = (
+    <Stack.Navigator screenOptions={{headerShown: false}}>
+      <Stack.Screen name="LoginScreen" component={LoginScreen} />
+    </Stack.Navigator>
+  );
+  return isLogin ? MainRouter : AuthRouter;
+};
+
+export default Router;
