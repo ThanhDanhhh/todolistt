627bb4ab5022e0a7d5a8fb0311f3482a8e734a0d
thanhdanhdev <danhdang270@gmail.com>
2024-08-20 09:56:21 +0700

    add searchkey

--- src/screens/tasks/ListTasks.tsx
+++ src/screens/tasks/ListTasks.tsx
@@ -1,18 +1,54 @@
 import {View, Text, FlatList, TouchableOpacity} from 'react-native';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Container from '../../components/container';
 import TextComponent from '../../components/TextComponent';
 import TitleComponent from '../../components/TitleComponent';
 import {TaskModel} from '../../models/TaskModel';
+import SectionComponet from '../../components/SectionComponet';
+import InputComponent from '../../components/InputComponent';
+import {SearchNormal, SearchNormal1} from 'iconsax-react-native';
+import {colors} from '../../contants/colors';
 
 const ListTasks = ({navigation, route}: any) => {
   const {tasks}: {tasks: TaskModel[]} = route.params;
 
+  const [searchKey, setSearchKey] = useState('');
+  const [results, setResults] = useState<TaskModel[]>([]);
+
+  useEffect(() => {
+    if (!searchKey) {
+      setResults([]);
+    } else {
+      const items = tasks.filter(element =>
+        element.title
+          .toLocaleLowerCase()
+          .includes(searchKey.toLocaleLowerCase()),
+      );
+      setResults(items);
+    }
+  }, [searchKey]);
+
   return (
     <Container back>
-      <TextComponent text="Hello," />
+      <SectionComponet>
+        <InputComponent
+          value={searchKey}
+          onChange={val => setSearchKey(val)}
+          allowClear
+          prefix={<SearchNormal1 size={20} color={colors.gray2} />}
+          placeholder="Search"
+        />
+      </SectionComponet>
+
       <FlatList
-        data={tasks}
+        style={{flex: 1}}
+        showsVerticalScrollIndicator={false}
+        data={searchKey ? results : tasks}
+        ListEmptyComponent={
+          <SectionComponet>
+            <TextComponent text="Data not found!!!!" />
+          </SectionComponet>
+        }
         renderItem={({item}) => (
           <TouchableOpacity
             style={{
@@ -22,7 +58,7 @@ const ListTasks = ({navigation, route}: any) => {
             onPress={() => navigation.navigate('TaskDetail', {id: item.id})}
             key={item.id}>
             <TitleComponent text={item.title} />
-            <TextComponent text={item.description} line={3} />
+            <TextComponent text={item.description} line={2} />
           </TouchableOpacity>
         )}
       />
